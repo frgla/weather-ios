aa16884341a62ad389a82b80c09e9891f50a7fe1
Gauss <fran.glavota@gauss.hr>
2019-05-28 10:45:02 +0200

    added single table view cell

---
+++ TableViewCells/SingleTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SingleTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Gauss on 28/05/2019.
+//  Copyright © 2019 Fran Glavota. All rights reserved.
+//
+
+import UIKit
+
+class SingleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dayOfTheWeek: UILabel!
+    @IBOutlet weak var tempMinLabel: UILabel!
+    @IBOutlet weak var tempMaxLabel: UILabel!
+    @IBOutlet weak var weatherIconImage: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    final func config (with data: forecastItem) {
+        dayOfTheWeek.text = data.day
+        tempMinLabel.text = String(data.tempLow)
+        tempMaxLabel.text = String(data.tempHigh)
+        summaryLabel.text = data.summary
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
